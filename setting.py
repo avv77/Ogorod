01616df5cbacb095bacdbff800d12e20a95e729e
avv77 <mag7777@mail.ru>
2022-06-05 19:26:39 +0300

    05-06-2022-154

--- setting.py
+++ setting.py
@@ -1,15 +1,6 @@
 # -*- coding: utf-8 -*-
 import telebot
 
+bot = telebot.TeleBot('BOT_TOKEN')
 
-BOT_TOKEN = '5273429737:AAH-NYcU0OYnnlw1AsIjHJDaIMcYrgodI6w'
 
-bot = telebot.TeleBot(BOT_TOKEN)
-
-CHANNEL_NAME = '@sad_ogorod_2022'
-
-path = r'/app/files/Plan.xlsx'
-
-time1 = "06:00:00"
-time2 = "09:00:00"
-time3 = "15:00:00"
